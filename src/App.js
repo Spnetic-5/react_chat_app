1836663a9185178fea98cc3d3dc6e51db4adfd64
Saurabh Suresh Powar <66636289+Spnetic-5@users.noreply.github.com>
2021-03-22 01:33:03 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css'
-
+import ChatFeed from './components/ChatFeed';
 const App = () => {
-    return ( <
-        ChatEngine height = '100vh'
-        projectID = ""
-        userName = ""
-        userSecret = "" /
-        >
-    );
-}
+        return ( < ChatEngine height = '100vh'
+            userName = 'Spnetic-5'
+            userSecret = 'Pransau511'
+            projectID = '2b719952-c365-46bf-b04d-898e14577138'
+            renderChatFeed = {
+                (chatAppProps) => < ChatFeed {...chatAppProps }
+                />}  / >
+            );
+        }
 
-export default App;
+        export default App;
