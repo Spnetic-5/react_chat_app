123b3fff517e43fd5db18cd6abefb8841c0486f9
Saurabh Suresh Powar <66636289+Spnetic-5@users.noreply.github.com>
2021-03-23 01:07:31 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css'
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 const App = () => {
-        return ( < ChatEngine height = '100vh'
-            userName = 'Spnetic-5'
-            userSecret = 'Pransau511'
-            projectID = '2b719952-c365-46bf-b04d-898e14577138'
-            renderChatFeed = {
-                (chatAppProps) => < ChatFeed {...chatAppProps }
-                />}  / >
-            );
-        }
+        if (!localStorage.getItem('username')) return <LoginForm / >
+
+            return ( < ChatEngine height = '100vh'
+                userName = { localStorage.getItem('username') }
+                userSecret = { localStorage.getItem('password') }
+                projectID = '2b719952-c365-46bf-b04d-898e14577138'
+                renderChatFeed = {
+                    (chatAppProps) => < ChatFeed {...chatAppProps }
+                    />}
+                    onNewMessage = {
+                        () => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()
+                    }
+                    / >
+                );
+            };
 
         export default App;
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -5,28 +5,20 @@ const LoginForm = () =>{
     const [username,setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-const handleSubmit = (e) => {
+const handleSubmit = async (e) => {
     e.preventDefault();
-    const authobject = { 'Project-ID': '2b719952-c365-46bf-b04d-898e14577138', 'User-Name': username, 'User-Secret':password };
+    const authObject = { 'Project-ID': '2b719952-c365-46bf-b04d-898e14577138', 'User-Name': username, 'User-Secret':password };
     try {
         // username | password => chatengine -> give messages
-        await axios.get('https://api.chatengine.io/chats', {headers : authobject});
-        localStorage.setItem('username', username)
-        localStorage.setItem('password', password)
-        window.location.reload()
-        
-
+        await axios.get('https://api.chatengine.io/chats', {headers : authObject});
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+        window.location.reload();
         // works out -> logged in
 
     }catch(error) {
         // error -> Try with new username
-    }
-
-
-
-    
-   
-    
+    }  
 }
 
 return(
@@ -35,7 +27,7 @@ return(
             <h1 className="title">Chat App</h1>
             <form onSubmit={handleSubmit}></form>
             <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
-            <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
             <div align="center">
                 <button type="submit" className="button"  >
                     <span>Start Chatting</span>
@@ -46,3 +38,6 @@ return(
 )
 
 }
+
+
+export default LoginForm;
